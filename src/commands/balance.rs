db23008f68c22abe0a90d7f10221ec95900e0b9e
Karl <gruenesgnu@posteo.de>
2022-10-05 20:22:23 +0200

    Clearer balance output

--- src/commands/balance.rs
+++ src/commands/balance.rs
@@ -5,13 +5,13 @@ use crate::model::{blocked, entry, hours};
 
 pub fn execute(connection: &mut SqliteConnection) {
     let oldest_date = hours::get_oldest_date(connection);
-    println!("{}", oldest_date);
+    println!("Summing entries starting from {}", oldest_date);
 
     let now = Local::now().naive_local();
 
-    let sum_clocked = entry::getSumFor(&oldest_date, &now, connection);
-    let sum_blocked = blocked::getSumFor(&oldest_date, &now, connection);
+    let sum_clocked = entry::get_sum_for(&oldest_date, &now, connection);
+    let sum_blocked = blocked::get_sum_for(&oldest_date, &now, connection);
 
     let sum = sum_clocked + sum_blocked;
-    println!("{:.2}", sum)
+    println!("So far you tracked \n{:.2} hours.", sum)
 }
